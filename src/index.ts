685f442b7172400a1fd3e39e5fb19089a925a1a1
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-02-19 18:08:00 -0600

    express setting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req, res)=>{
+app.use(router);
 
-})
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
